dbf7e5981856b2fb98e0dd24dd014d03a975f7f8
Thịnh Nguyễn <dst15092004@gmail.com>
2024-03-22 08:50:57 +0700

    a

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
             <img src="assets/imgs/icons.png" alt="ABC Hospital">
             <h3>Welcome to ABC hospital!</h3>
         </div>
-        <div class="login-box">
+        <div class="login-box" id="login">
             <ul class="nav nav-tabs nav-justified" role="tablist">
                 <div class="slider"></div>
                 <li class="nav-item">
@@ -118,7 +118,26 @@
     </div>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+    <script>
+    $(document).ready(function(){
+                      
+        $("#login .nav-tabs a").click(function() {
+      
+        var position = $(this).parent().position();
+      
+        var width = $(this).parent().width();
+      
+        $("#login .slider").css({"left":+ position.left,"width":width});
+      
+        });
+        var actWidth = $("#login .nav-tabs").find(".active").parent("li").width();
+    
+        var actPosition = $("#login .nav-tabs .active").position();
+    
+        $("#login .slider").css({"left":+ actPosition.left,"width": actWidth});
+    
+        });        
+    </script>    
 </body>
 <?php
     if(isset($_SESSION['message'])) echo $_SESSION['message'];
