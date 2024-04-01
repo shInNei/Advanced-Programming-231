c4bb30b07042a9f26270fc53024d06df3699b363
Enigmask22 <116486513+Enigmask22@users.noreply.github.com>
2024-04-01 23:55:50 +0700

    1

--- admin/medicine/medicineDashboard.php
+++ admin/medicine/medicineDashboard.php
@@ -2,6 +2,8 @@
 require_once('../../includes/header.php');
 ?>
 <link rel="stylesheet" href="../../assets/css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
 </head>
 
 <body>
@@ -26,10 +28,9 @@ require_once('../../includes/header.php');
         </div>
     </nav>
 
-    <div class="content-wrap">
+    <!-- <div class="content-wrap">
         <div class="container login">
             <div class="row login-image">
-                <!-- <img src="assets/imgs/icons.png" alt="ABC Hospital"> -->
                 <h3>Welcome Admin!</h3>
             </div>
             <div class="container dashboard" id="med-dashboard">
@@ -42,8 +43,31 @@ require_once('../../includes/header.php');
                     </div>
                 </div>
             </div>
+            
+        </div>
+    </div> -->
+
+    <div class="container login" style="font-family: 'IBM Plex Sans', sans-serif;">
+        <div class="row login-image">
+            <h3>Welcome Admin!</h3>
         </div>
     </div>
-    <?php
-    require_once('../../includes/footer.php');
-    ?>
+    <div class="container mt-5">
+        <div class="container text-center">
+            <div class="row row-cols-4 justify-content-center">
+                <a href="addMed.php" class="col btn btn-success m-2 py-4">
+                    <i class="fa fa-pills" style="font-size: 40px;"></i><br>
+                    Medicine
+                </a>
+                <a href="../equipment/addEquip.php" class="col btn btn-success m-2 py-4">
+                    <i class="fa-solid fa-list" style="font-size: 40px;"></i><br>
+                    Equipment
+                </a>
+            </div>
+        </div>
+    </div>
+    <footer class="text-center text-dark fixed-bottom">
+        Copyright &copy; 2024 ABC Hospital. All rights reserved.
+    </footer>
+
+</body>
--- includes/header.php
+++ includes/header.php
@@ -8,4 +8,5 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-vZzbBsnHH2MfLTDi0EKNLOtptY7X6lA8/YH8I1rr7OZBozXs/lNL6p5TRbgKwRzY4cgADGUWZzY/YG9zqo06Fg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.0.13/css/all.css'>
+    
     <title>ABC HOSPITAL</title>
