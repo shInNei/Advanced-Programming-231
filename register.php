40eb6139458f94c2dd5049cd89e483a3412968de
mahara0511 <122422522+mahara0511@users.noreply.github.com>
2024-04-02 07:40:21 +0700

    Add files via upload

--- register.php
+++ register.php
@@ -1,5 +1,25 @@
+<?php
+    session_start();
+?>
+
+<?php
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "hospital";
+    $conn = "";
+    try{
+        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    }
+    catch (mysqli_sql_exception){
+        echo "Failed connection";
+    } 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
+
 
 <head>
     <meta charset="UTF-8">
@@ -7,124 +27,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/register.css" />
+    <link rel="stylesheet" href="assets/css/register.css">
+    <link rel="stylesheet" href="assets/css/register_minhkhanh.css">
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.0.13/css/all.css'>
+    <link rel='stylesheet' href="assets/css/">
     <title>ABC HOSPITAL</title>
 </head>
 
 <body>
-    <style>
-        .my-custom-row {
-            background-color: bisque;
-            height: 400px;
-        }
-        body{
-            background-color: white;   
-            margin: 0;
-        }
-        .Register_box{
-            position: relative;
-            margin: auto;
-            background-color: white;
-            max-width: 700px;
-        }
 
-        .Register_sentence{
-
-            font-size: 30px;
-            text-align: center;
-            color: rgb(73, 80, 87);
-        }
-        .input{
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            column-gap: 30px;
-            row-gap: 15px;
-
-        }
-        .INPUT_INFOR{
-            box-sizing: border-box;
-            padding-left: 10px;
-            width: 100%;
-            height: 40px;
-            border-radius: 10px;
-            font-size: 15px;
-
-        }
-        .Female_checkbox{
-            border-radius: 50%;
-        }
-        .Male_box,
-        .Female_box{
-            display: flex;
-        }
-        .Female_box{
-            margin-left: 10px;
-        }
-        .gender_checkbox{
-            display: inline-block;
-            display:flex;
-        }
-        .Have_an_account{
-            margin-top:0;
-            color:rgb(0, 123, 255);
-            display: inline-block;
-            cursor: pointer;
-        }
-        .Have_an_account:hover{
-            text-decoration: underline;
-        }
-        .Under_Register_Block{
-            display: flex;
-            justify-content: space-between;
-        }
-        .Register_button{
-            border-radius: 30px;
-            background-color: rgb(3, 126, 106);
-            color: white;
-            width: 120px;
-            height: 40px;
-            font-size: 20px;
-            cursor: pointer;
-            transition: opacity 0.3;
-            margin-top: 60px;
-            margin-left: 20px;
-        }
-        .Register_button:hover{
-            opacity: 0.7;
-        }
-        .Register_button:active{
-            background-color: antiquewhite;
-            color: red;
-        }
-        .iconYTe{
-            border-radius: 50%;
-            width: 50px;
-            margin-right: 10px;
-        }
-        .left_header{
-            display: flex;
-        }
-        .right_header{
-            display: flex;
-        }
-        .header{
-            background-color:white;
-            font-size: 21px;
-            display: flex;
-            justify-content: space-between;
-            margin-bottom: 70px;
-            margin-top:0px;
-        }
-        .header_content_box{
-            padding-left: 10px;
-            padding-right:10px;
-            cursor: pointer;
-        }
-        .header_content_box:hover{
-            background-color: rgb(3, 126, 106);
-        }
-    </style>
     <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
         <div class="container main-nav">
             <a class="navbar-brand" href="#" >
@@ -166,40 +77,42 @@
                     <p class = "Register_sentence">Register As Patient</p>
                 </div>
                 <div class = "input">
-                    <div> <input class = "INPUT_INFOR" placeholder="First Name *"> </div>
-                    <div> <input class = "INPUT_INFOR" placeholder="Last Name *"> </div>
-                    <div> <input class = "INPUT_INFOR" placeholder="Your Email *"></div>
-                    <div> <input class = "INPUT_INFOR" placeholder="Your Phone *"></div>
-                    <div> <input class = "INPUT_INFOR" placeholder="Password *"></div>
-                    <div> <input class = "INPUT_INFOR" placeholder="Confirm Your Password *"></div>
+
+                        <div> <input class = "INPUT_INFOR" placeholder="First Name *" name = "first_name" type = "text"> </div>
+                        <div> <input class = "INPUT_INFOR" placeholder="Last Name *" name = "last_name" type = "text"> </div>
+                        <div> <input class = "INPUT_INFOR" placeholder="Your Email *" name = "email" type = "text"></div>
+                        <div> <input class = "INPUT_INFOR" placeholder="Your Phone *" name = "phone_number" type = "text" ></div>
+                        <div> <input class = "INPUT_INFOR" placeholder="Password *" name = "password" type = "password"></div>
+                        <div> <input class = "INPUT_INFOR" placeholder="Confirm Your Password *" name = "confirm_password" type = "password"></div>
+
                 </div>
                 <div class = "Under_Register_Block">
                     <div class ="Left_Under_Register_Block">
                         <div class = "gender_checkbox">
                             <div class = "Male_box">
-                                <input class = "Male_checkbox" placeholder="Male_checkbox" type = "checkbox">
-                                <p>Male</p>
+                                <input class = "Male_checkbox" placeholder="Male_checkbox" type = "radio" name = "gender" value = "M">
+                                <p class = "Male_text" >Male</p>
                             </div>
             
                             <div class = "Female_box">
-                                <input class = "Female_checkbox" placeholder ="Female_checkbox" type = "checkbox">
-                                <p>Female</p>
+                                <input class = "Female_checkbox" placeholder ="Female_checkbox" type = "radio" name = "gender" value = "F">
+                                <p class = "Female_text" >Female</p>
                           
                             </div>
                         </div>
-                        <p class = "Have_an_account"> Already have an account?</p>
+                        <a href = "index.php" class = "Have_an_account"> Already have an account?</a>
     
                     </div>
                     
                     <div class = "Right_Under_Register_Block">
-                        <button class = "Register_button">Register</button>
+                        <button class = "Register_button" name = "register">Register</button>
                     </div>
                 </div>
             </form>
         </section>
-        <div class="text-center text-dark">
+        <footer class="text-center text-dark">
             Copyright &copy; 2024 ABC Hospital. All rights reserved.
-        </div>
+        </footer>
     </div>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -207,5 +120,80 @@
 
 </body>
 
-
 </html>
+
+<?php 
+    if(isset($_POST["register"])){
+        
+        $gender = "";
+        $firstname = filter_input(INPUT_POST, "first_name", FILTER_SANITIZE_SPECIAL_CHARS);
+        $lastname = filter_input(INPUT_POST, "last_name", FILTER_SANITIZE_SPECIAL_CHARS);
+        $email_check = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+        $phone = filter_input(INPUT_POST, "phone_number", FILTER_SANITIZE_NUMBER_INT);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+        $confirm_password = filter_input(INPUT_POST, "confirm_password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+
+        $error_message = "";
+        if(isset($_POST["gender"])){
+            $gender = $_POST["gender"];
+        }
+
+        if(empty($firstname) || empty($lastname) || empty($email) || empty($phone) || empty($confirm_password) || empty($password) || empty($gender)){
+            $error_message = "Please input all the information";
+        } 
+        elseif(!$email_check) {
+            $error_message = "Email is not valid";
+        } elseif($password != $confirm_password) {
+            $error_message = "Passwords are different"; 
+        } else {
+            $sql = "SELECT * FROM patients WHERE email = '$email'";
+            $result = $conn->query($sql);
+
+            if($result->num_rows > 0){
+                $error_message = "Email already exists, please choose another email";
+            }
+            else {
+                $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    
+                $sql = "INSERT INTO patients (fName, lName, email, phoneNum, gender, pw)
+                        VALUES ('$firstname', '$lastname', '$email', '$phone', '$gender', '$hashed_password')";
+
+                $result = mysqli_query($conn, $sql);
+
+                // if($result)
+                // {
+                //     echo '<script type="text/javascript">'; 
+                //     echo 'alert("Dang ki thanh cong!");'; 
+                //     echo 'window.location.href = "index.php";';
+                //     echo '</script>';
+                // } 
+                if($result){
+                    mysqli_query($conn, $sql);
+                    mysqli_close($conn);
+                    $error_message = "Dang ki thanh cong";
+                    header("location: index.php");
+                }
+                
+                // catch (mysqli_sql_exception){
+                //     $error_message = "Dang ki that bai";    
+                // }
+    
+                // mysqli_close($conn);
+                   
+                
+                // $_SESSION["email"] = $email;
+                // $_SESSION["password"] = $password;
+                
+            }  
+        }
+        
+    }
+    if (!empty($error_message)) {
+        echo '<script type="text/javascript">'; 
+        echo 'alert("' . $error_message . '");'; 
+        echo 'window.location.href = "register.php";';
+        echo '</script>';
+    }
+?>
