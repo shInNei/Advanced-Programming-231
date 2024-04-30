617be157ccbd5ffe50f58b6f5d0abb3dbf9b60e2
Khoawawa <atom08072004@gmail.com>
2024-04-30 16:21:14 +0700

    a

--- classes/Dbh.php
+++ classes/Dbh.php
@@ -3,9 +3,9 @@ class Dbh
 {
     protected $conn;
     private $host = "localhost";
-    private $userName = "id22104051_hospital";
+    private $userName = "id22104283_hospital";
     private $pw = "Abc@123@";
-    private $dbName = "id22104051_hospital";
+    private $dbName = "id22104283_hospital";
     public function __construct()
     {
         try {
